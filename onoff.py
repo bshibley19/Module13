2656e53fee23e9eb495dbf9b48b8bda9c8e2d99b
bshibley19 <32138092+bshibley19@users.noreply.github.com>
2017-12-13 10:24:07 -0600

    Update onoff.py

--- onoff.py
+++ onoff.py
@@ -11,6 +11,9 @@ RPL.servoWrite(motorR, 1000)
 RPL.servoWrite(motorL, 2000)
 RPL.pinMode(sensor_pin,RPL.INPUT)
 
-if RPL.digitalRead(sensor_pin) == 0:
-    RPL.servoWrite(motorR, 0)
-    RPL.servoWrite(motorL, 0)
+while RPL.digitalRead(sensor_pin) == 0:
+    RPL.servoWrite(motorR, 1000)
+    RPL.servoWrite(motorL, 2000)
+    else:
+        RPL.servoWrite(motorR, 0)
+        RPL.servoWrite(motorL, 0)
