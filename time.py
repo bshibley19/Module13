febf31b9765cc1c5f2dd922141f556e67903dbf4
bshibley19 <32138092+bshibley19@users.noreply.github.com>
2017-12-14 09:05:59 -0600

    Update time.py

--- time.py
+++ time.py
@@ -5,8 +5,16 @@ RPL.RoboPiInit("/dev/ttyAMA0",115200)
 
 motorL = 1
 motorR = 0
-t_end = time.time() + 0.3
+t_end = time.time() + 3
+t_max = time.time() + 7
 
 while time.time() < t_end:
     RPL.servoWrite(motorR, 1000)
     RPL.servoWrite(motorL, 2000)
+
+while time.time() == range(t_end,t_max):
+    RPL.servoWrite(motorR, 0)
+    RPL.servoWrite(motorL, 0)
+
+while time.time() > 6:
+    reload(time.time())
