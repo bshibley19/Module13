bcb40c8c45492b56b5f0677cc97e642d731bff51
bshibley19 <32138092+bshibley19@users.noreply.github.com>
2017-12-18 10:19:27 -0600

    Update time.py

--- time.py
+++ time.py
@@ -7,10 +7,9 @@ motorL = 1
 motorR = 0
 t_end = time.time() + 3
 t_max = time.time() + 7
-x = 1
-y = 1
 
-while x + y == 2:
+while 1 + 1 == 2:
+
     while time.time() < t_end:
         RPL.servoWrite(motorR, 1000)
         RPL.servoWrite(motorL, 2000)
