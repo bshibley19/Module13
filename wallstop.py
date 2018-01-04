58e84b8244badbc7c8d98b5a04e4a009c34dab9b
bshibley19 <32138092+bshibley19@users.noreply.github.com>
2018-01-04 14:27:36 -0600

    Update wallstop.py

--- wallstop.py
+++ wallstop.py
@@ -14,19 +14,37 @@ RPL.RoboPiInit("/dev/ttyAMA0",115200)
 motorL = 1
 motorR = 0
 sensor_pin = 24
+motorR_forward = 2000
+motorL_forward = 1000
 
 RPL.servoWrite(motorR, 2000)
 RPL.servoWrite(motorL, 1000)
 RPL.pinMode(sensor_pin,RPL.INPUT)
 
+def forwardSpeedChanges(change, mn = 1600, mx = 2900):
+  global motorR_forward
+  global motorL_forward
+  motorR_forward += change
+  motorL_forward += change
+  motorR_forward = max(min(motorR_forward, mx), mn)
+  motorL_forward = max(min(motorL_forward, mx), mn)
+  print_speed()
+
 if RPL.digitalRead(sensor_pin) == 0:
     while RPL.digitalRead(sensor_pin) == 0:
         while time.time() < time.time() + 0.2:
-            RPL.servoWrite(motorR, 1750)
-            RPL.servoWrite(motorL, 750)
+            forwardSpeedChanges(-100)
         while time.time() < time.time() + 0.4:
-            RPL.servoWrite(motorR, 1500)
-            RPL.servoWrite(motorL, 500)
+            forwardSpeedChanges(-500)
         while time.time() < time.time() + 0.6:
             RPL.servoWrite(motorR, 0)
             RPL.servoWrite(motorL, 0)
+
+def forwardSpeedChanges(change, mn = 1600, mx = 2900):
+  global motorR_forward
+  global motorL_forward
+  motorR_forward += change
+  motorL_forward += change
+  motorR_forward = max(min(motorR_forward, mx), mn)
+  motorL_forward = max(min(motorL_forward, mx), mn)
+  print_speed()
