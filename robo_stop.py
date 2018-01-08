ecce2a5d408136559af7804d84f5fd278d285a01
bshibley19 <32138092+bshibley19@users.noreply.github.com>
2018-01-08 10:14:47 -0600

    Update robo_stop.py

--- robo_stop.py
+++ robo_stop.py
@@ -3,8 +3,10 @@ from setup import RPL
 RPL.RoboPiInit("/dev/ttyAMA0",115200)
 
 print "Please type in the pin number of your motors."
-motorL = int(raw_input("Left Motor > "))
-motorR = int(raw_input("Right Motor > "))
+b = int(raw_input("Left Motor > "))
+c = int(raw_input("Right Motor > "))
+motorL = b
+motorR = c
 
 
 RPL.servoWrite(motorR, 0)
