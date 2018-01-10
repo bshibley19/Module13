765c3ff0171dc7a39fc7bee4b0d7bdf2315de747
bshibley19 <32138092+bshibley19@users.noreply.github.com>
2018-01-10 09:59:57 -0600

    Update wallstop.py

--- wallstop.py
+++ wallstop.py
@@ -7,8 +7,10 @@ motorL = 1
 motorR = 7
 sensor_pin = 16
 move = time.time()
-i = time.time() + 0.4
-j = time.time() + 0.7
+i = 0.4
+j = 0.7
+slowR = RPL.servoWrite(motorR, 1480)
+slowL = RPL.servoWrite(motorL, 1520)
 
 RPL.servoWrite(motorR, 2000)
 RPL.servoWrite(motorL, 1000)
@@ -16,8 +18,8 @@ RPL.pinMode(sensor_pin,RPL.INPUT)
 
 if RPL.digitalRead(sensor_pin) == 0:
     while time.time() < move + i:
-        RPL.servoWrite(motorR, 1380)
+        RPL.servoWrite(motorR, 1480)
         RPL.servoWrite(motorL, 1520)
-    while time.time() < move + j:
+    while time.time() > move + i:
         RPL.servoWrite(motorR, 0)
         RPL.servoWrite(motorL, 0)
