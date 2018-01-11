e4e211c429f8bf385a7ce1c1865b6f23309e1d59
bshibley19 <32138092+bshibley19@users.noreply.github.com>
2018-01-11 14:16:20 -0600

    Create wallturn.py

---
+++ wallturn.py
@@ -0,0 +1,26 @@
+import RoboPiLib as RPL
+from setup import RPL
+import time
+RPL.RoboPiInit("/dev/ttyAMA0",115200)
+
+motorL = 1
+motorR = 7
+sensor_pin = 16
+move = time.time()
+j = 3
+i = 4.9
+
+while RPL.digitalRead(sensor_pin) == 1:
+    RPL.servoWrite(motorR, 1000)
+    RPL.servoWrite(motorL, 2000)
+    if RPL.digitalRead(sensor_pin) != 1:
+        break
+
+
+while RPL.digitalRead(sensor_pin) == 0:
+    while time.time() < (move + i):
+        RPL.servoWrite(motorR, 1490)
+        RPL.servoWrite(motorL, 1520)
+    while time.time() > (move + i):
+        RPL.servoWrite(motorR, 0)
+        RPL.servoWrite(motorL, 0)
