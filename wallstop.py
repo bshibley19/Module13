0807fd230809496e9d61b69d2ed5a8d01212780a
bshibley19 <32138092+bshibley19@users.noreply.github.com>
2018-01-11 14:00:01 -0600

    Update wallstop.py

--- wallstop.py
+++ wallstop.py
@@ -19,7 +19,7 @@ while RPL.digitalRead(sensor_pin) == 1:
 
 while RPL.digitalRead(sensor_pin) == 0:
     while time.time() < (move + i):
-        RPL.servoWrite(motorR, 1480)
+        RPL.servoWrite(motorR, 1475)
         RPL.servoWrite(motorL, 1520)
     while time.time() > (move + i):
         RPL.servoWrite(motorR, 0)
