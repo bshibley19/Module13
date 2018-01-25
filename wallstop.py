3568746322eba26ac2fe1970c0249db3d0637db3
bshibley19 <32138092+bshibley19@users.noreply.github.com>
2018-01-25 09:04:27 -0600

    Update wallstop.py

--- wallstop.py
+++ wallstop.py
@@ -10,9 +10,7 @@ sensor_pin = 16
 i = 4.9
 
 while RPL.digitalRead(sensor_pin) == 1:
-    PTW.state['d0'] = RPL.digitalRead(sensor_pin)
-    PTW.state['m1'] = RPL.digitalRead(sensor_pin)
-    PTW.state['m7'] = RPL.digitalRead(sensor_pin)
+    PTW.state['d1'] = RPL.digitalRead(sensor_pin)
     RPL.servoWrite(motorR, 1000)
     RPL.servoWrite(motorL, 2000)
     PTW.post()
@@ -20,18 +18,12 @@ while RPL.digitalRead(sensor_pin) == 1:
         break
 
 while RPL.digitalRead(sensor_pin) == 0:
-    PTW.state['d0'] = RPL.digitalRead(sensor_pin)
+    PTW.state['d1'] = RPL.digitalRead(sensor_pin)
     move = time.time()
-    PTW.post()
+     PTW.post()
     while time.time() < (move + i):
-        PTW.state['m1'] = RPL.digitalRead(sensor_pin)
-        PTW.state['m7'] = RPL.digitalRead(sensor_pin)
         RPL.servoWrite(motorR, 1475)
         RPL.servoWrite(motorL, 1520)
-        PTW.post()
     while time.time() > (move + i):
-        PTW.state['m1'] = RPL.digitalRead(sensor_pin)
-        PTW.state['m7'] = RPL.digitalRead(sensor_pin)
         RPL.servoWrite(motorR, 0)
         RPL.servoWrite(motorL, 0)
-        PTW.post()
