e9869c24a3aa5ba83e1b4312963bbc25f0107498
bshibley19 <32138092+bshibley19@users.noreply.github.com>
2018-01-30 09:24:26 -0600

    Update wallstop.py

--- wallstop.py
+++ wallstop.py
@@ -14,7 +14,7 @@ while RPL.digitalRead(sensor_pin) == 1:
     RPL.servoWrite(motorR, 1000)
     RPL.servoWrite(motorL, 2000)
     PTW.post()
-    time.sleep(0.23)
+    time.sleep(0.25)
     if RPL.digitalRead(sensor_pin) != 1:
         break
 
