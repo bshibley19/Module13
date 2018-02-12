ae30a609711872bb3330951e603826e147884d98
bshibley19 <32138092+bshibley19@users.noreply.github.com>
2018-02-12 09:23:47 -0600

    Update wallturn.py

--- wallturn.py
+++ wallturn.py
@@ -5,30 +5,40 @@ RPL.RoboPiInit("/dev/ttyAMA0",115200)
 
 motorL = 1
 motorR = 7
-sensor_pin = 16
+sensor_R = 16
+sensor_M = 17
+sensor_L = 18
 j = 3
-i = 5.5
+i = 4.0
 
-while RPL.digitalRead(sensor_pin) == 1:
+while RPL.digitalRead(sensor_R) and RPL.digitalRead(sensor_M) and RPL.digitalRead(sensor_L)== 1:
     PTW.state['d1'] = RPL.digitalRead(sensor_pin)
     RPL.servoWrite(motorR, 1000)
     RPL.servoWrite(motorL, 2000)
     PTW.post()
-    if RPL.digitalRead(sensor_pin) != 1:
+    if RPL.digitalRead(sensor_R) or RPL.digitalRead(sensor_M) or RPL.digitalRead(sensor_L) =! 1:
         break
 
-while RPL.digitalRead(sensor_pin) == 0:
+while RPL.digitalRead(sensor_R) == 0:
     PTW.state['d1'] = RPL.digitalRead(sensor_pin)
     move = time.time()
     while time.time() < (move + i):
-        PTW.state['m1'] = RPL.digitalRead(sensor_pin)
-        PTW.state['m7'] = RPL.digitalRead(sensor_pin)
         RPL.servoWrite(motorR, 1490)
         RPL.servoWrite(motorL, 1520)
         PTW.post()
     while time.time() > (move + i):
-        PTW.state['m1'] = RPL.digitalRead(sensor_pin)
-        PTW.state['m7'] = RPL.digitalRead(sensor_pin)
         RPL.servoWrite(motorR, 1460)
         RPL.servoWrite(motorL, 1550)
+        PTW.post()
+
+while RPL.digitalRead(sensor_L) == 0:
+    PTW.state['d1'] = RPL.digitalRead(sensor_pin)
+    run = time.time()
+    while time.time() < (run + i):
+        RPL.servoWrite(motorR, 1520)
+        RPL.servoWrite(motorL, 1490)
+        PTW.post()
+    while time.time() > (run + i):
+        RPL.servoWrite(motorR, 1550)
+        RPL.servoWrite(motorL, 1460)
         PTW.post()
