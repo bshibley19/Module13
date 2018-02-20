3537dbf7762713d8358e84f729ab0689ea80e230
bshibley19 <32138092+bshibley19@users.noreply.github.com>
2018-02-20 09:13:35 -0600

    Analog Testing

--- wallfollow.py
+++ wallfollow.py
@@ -2,3 +2,8 @@ import RoboPiLib as RPL
 import time
 import post_to_web as PTW
 RPL.RoboPiInit("/dev/ttyAMA0",115200)
+
+sensor_pin = 27
+RPL.pinMode(sensor_pin,RPL.INPUT)
+
+RPL.analogRead(sensor_pin)
