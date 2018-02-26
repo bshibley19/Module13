a2cac11de249616648bc258ea6131df65e17331e
bshibley19 <32138092+bshibley19@users.noreply.github.com>
2018-02-26 09:30:03 -0600

    Update wallfollow.py

--- wallfollow.py
+++ wallfollow.py
@@ -3,3 +3,29 @@ import time
 import post_to_web as PTW
 RPL.RoboPiInit("/dev/ttyAMA0",115200)
 
+motorL = 1
+motorR = 7
+sensor_L = 16
+sensor_M = 17
+sensor_R = 18
+analog_1 = 0
+j = 3
+i = 4.0
+
+while RPL.analogRead(analog_1) > 420 and RPL.analogRead(analog_1) < 620:
+    RPL.servoWrite(motorR, 1000)
+    RPL.servoWrite(motorL, 2000)
+    if RPL.digitalRead(sensor_R) != 1 or RPL.analogRead < 420:
+        break
+
+while RPL.analogRead(analog_1) < 420 and RPL.analogRead(analog_1) > 200:
+    move = time.time()
+    while timetime() < (move + 1):
+        RPL.servoWrite(motorR, 1530)
+        RPL.servoWrite(motorL, 1460)
+    while time.time() > (move + 1):
+        break
+    
+while RPL.analogRead(analog_1) < 200:
+    RPL.servoWrite(motorR, 0)
+    RPL.servoWrite(motorL, 0)
